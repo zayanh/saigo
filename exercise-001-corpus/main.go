a3ec38ab2da1b3a663b4cc5133e64a21fb0a2219
zayanh <zayan.hafeez@gmail.com>
2016-07-14 11:55:27 +0100

    cleanup

--- exercise-001-corpus/main.go
+++ exercise-001-corpus/main.go
@@ -8,13 +8,6 @@ import (
 
 
 func main() {
-    var testEntries w.Entries
-    for i := 0; i < 10; i++ {
-        testEntries = append(testEntries, &w.Entry{Word: string(byte(0x61+i)), Count: i})
-    }
-    b := testEntries.Less(1,0)
-    fmt.Println(b)
-
     _, entries := w.WordCount(string(os.Args[1]))
 
     // Can add some error checking here based on result
